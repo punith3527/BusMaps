83b731f5df73f728283c5280cb6a72542a28891c
punith3527 <punithreddy7253@gmail.com>
2023-02-26 18:31:53 +0530

    Final No Billing

--- app/src/main/java/com/example/busmaps/MapsActivity.java
+++ app/src/main/java/com/example/busmaps/MapsActivity.java
@@ -1,15 +1,24 @@
 package com.example.busmaps;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,6 +27,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,11 +46,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
         // Set default location to Hyderabad, India
@@ -53,27 +67,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng jbsHyderabad = new LatLng(17.4477, 78.4980);
         mMap.addMarker(new MarkerOptions().position(jbsHyderabad).title("JBS"));
 
-// Create a list to store the waypoints
-        List<LatLng> waypoints = new ArrayList<>();
-
-// Create a polyline options object and set its properties
-        PolylineOptions polylineOptions = new PolylineOptions();
-        polylineOptions.color(Color.RED);
-        polylineOptions.width(5);
-        polylineOptions.geodesic(true);
-
-// Add the origin and destination to the waypoints list
-        waypoints.add(mgbsHyderabad);
-        waypoints.add(jbsHyderabad);
-
-// Draw the road route between the origin and destination
-        polylineOptions.addAll(waypoints);
-        mMap.addPolyline(polylineOptions);
-
         // Show labels on the markers
         mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
             @Override
-            public View getInfoContents(Marker marker) {
+            public View getInfoContents(@NonNull Marker marker) {
                 LinearLayout info = new LinearLayout(getApplicationContext());
                 info.setOrientation(LinearLayout.VERTICAL);
 
@@ -89,10 +86,90 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
             @Override
-            public View getInfoWindow(Marker marker) {
+            public View getInfoWindow(@NonNull Marker marker) {
                 return null;
             }
 
         });
+
+        // Get road route between MGBS and JBS points
+        String key = "AIzaSyAtrfs_lzcQMX0fRUUjkwsGk5fx3WXeZvw";
+        String directionURL = getDirectionURL(mgbsHyderabad, jbsHyderabad, key);
+
+        Log.e("url output", directionURL.toString());
+        // Draw road route on map
+        drawRoute(directionURL);
     }
+
+    // Get URL for getting road route between origin and destination points
+    private String getDirectionURL(LatLng origin, LatLng dest, String key) {
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin.latitude + "," + origin.longitude
+                + "&destination=" + dest.latitude + "," + dest.longitude
+                + "&sensor=false"
+                + "&mode=driving"
+                + "&key=" + key;
+    }
+
+    private void drawRoute(String url) {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    JSONArray routeArray = response.optJSONArray("routes");
+                    try {
+                        assert routeArray != null;
+                        JSONObject routes = routeArray.getJSONObject(0);
+                        JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+                        String encodedString = overviewPolylines.getString("points");
+                        List<LatLng> list = decodePoly(encodedString);
+                        PolylineOptions polylineOptions = new PolylineOptions()
+                                .addAll(list)
+                                .width(10)
+                                .color(Color.RED)
+                                .geodesic(true);
+                        mMap.addPolyline(polylineOptions);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> {
+                    Toast.makeText(getApplicationContext(), "Error occurred while drawing route", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, error.toString());
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+    private List<LatLng> decodePoly(String encodedString) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encodedString.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encodedString.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encodedString.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((double) lat / 1E5, (double) lng / 1E5);
+            poly.add(p);
+        }
+        return poly;
+    }
+
+
 }
